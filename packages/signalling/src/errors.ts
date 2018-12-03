f9faea09b02f6b0b637620da8a31efea86695401
Gregory Sims <briggybros.2@googlemail.com>
2018-12-03 19:11:56 +0000

    Add webrtc signalling server

---
+++ packages/signalling/src/errors.ts
@@ -0,0 +1,22 @@
+import { Socket } from 'socket.io';
+
+export enum Error {
+  INVALID_HOST_KEY = 'INVALID_HOST_KEY',
+  EXISTING_HOST = 'EXISTING_HOST',
+  SELF_HOST = 'SELF_HOST',
+  NO_HOST = 'NO_HOST',
+  NO_CONTROLLER = 'NO_CONTROLLER',
+}
+
+const messages: { [error: string]: string } = {
+  [Error.INVALID_HOST_KEY]: 'invalid host key',
+  [Error.EXISTING_HOST]: 'host already present',
+  [Error.SELF_HOST]: 'you are the host',
+  [Error.NO_HOST]: 'no host present',
+  [Error.NO_CONTROLLER]:
+    'controller with specified id does not exist or has disconnected',
+};
+
+export function sendError(socket: Socket, error: Error) {
+  return socket.emit('error', error, messages[error]);
+}
---
+++ packages/signalling/src/server.ts
@@ -0,0 +1,61 @@
+#!/bin/node
+import http from 'http';
+import socketIO, { Socket } from 'socket.io';
+
+import { Error, sendError } from './errors';
+
+const PORT: string = process.env.PORT || '8081';
+const HOST_KEY: string = process.env.HOST_KEY || '';
+
+const server = http.createServer();
+const io = socketIO(server);
+
+let host: Socket | null;
+
+io.on('connection', (socket: Socket) => {
+  /** HOST MESSAGES */
+  socket.on('register-host', hostKey => {
+    if (hostKey !== HOST_KEY) return sendError(socket, Error.INVALID_HOST_KEY);
+    if (host === null) return sendError(socket, Error.EXISTING_HOST);
+
+    host = socket;
+    host.on('disconnect', () => {
+      host = null;
+    });
+
+    host.on('host-offer', (controllerId, offer) => {
+      const controller = io.sockets.connected[controllerId];
+      if (!controller) return sendError(socket, Error.NO_CONTROLLER);
+      controller.emit('host-offer', offer);
+    });
+
+    host.on('host-ice-candidate', (controllerId, icecandidate) => {
+      const controller = io.sockets.connected[controllerId];
+      if (!controller) return sendError(socket, Error.NO_CONTROLLER);
+      controller.emit('host-ice-candidate', icecandidate);
+    });
+  });
+
+  /** CONTROLLER MESSAGES */
+  socket.on('controller-connect', () => {
+    if (host === null) return sendError(socket, Error.NO_HOST);
+    if (socket.id === host.id) return sendError(socket, Error.SELF_HOST);
+    host.emit('controller-connect', socket.id);
+  });
+
+  socket.on('controller-offer', offer => {
+    if (host === null) return sendError(socket, Error.NO_HOST);
+    if (socket.id === host.id) return sendError(socket, Error.SELF_HOST);
+    host.emit('controller-offer', socket.id, offer);
+  });
+
+  socket.on('controller-ice-candidate', icecandidate => {
+    if (host === null) return sendError(socket, Error.NO_HOST);
+    if (socket.id === host.id) return sendError(socket, Error.SELF_HOST);
+    host.emit('controller-ice-candidate', socket.id, icecandidate);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Signalling server listening on: ${PORT}`);
+});
