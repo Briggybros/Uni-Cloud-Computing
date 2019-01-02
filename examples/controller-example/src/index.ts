5b9dc6a0668e233ccd8fce9e56fb54600ad5d24e
Ben Norris <bn15932@my.bristol.ac.uk>
2019-01-02 15:58:00 +0000

    changed controllers for testing, added mouseupdown

--- examples/controller-example/src/index.ts
+++ examples/controller-example/src/index.ts
@@ -5,6 +5,12 @@ import getController, {
 
 const connectButton = document.getElementById('connect');
 const incrementButton = document.getElementById('increment');
+const upButton = document.getElementById('up');
+const downButton = document.getElementById('down');
+const leftButton = document.getElementById('left');
+const rightButton = document.getElementById('right');
+const submitButton = document.getElementById('submit');
+const cancelButton = document.getElementById('cancel');
 
 connectButton &&
   connectButton.addEventListener('click', async () => {
@@ -24,6 +30,48 @@ connectButton &&
     incrementButton &&
       incrementButton.addEventListener('click', () => {
         controller.send('increment');
+      });
+    upButton &&
+      upButton.addEventListener('mousedown', () => {
+        controller.send('up', 'true');
+      });
+      upButton.addEventListener('mouseup', () => {
+        controller.send('up', 'false');
+      });
+    downButton &&
+      downButton.addEventListener('mousedown', () => {
+        controller.send('down', 'true');
+      });
+      downButton.addEventListener('mousedown', () => {
+        controller.send('down', 'false');
+      });
+    leftButton &&
+      leftButton.addEventListener('mousedown', () => {
+        controller.send('left', 'true');
+      });
+      leftButton.addEventListener('mouseleft', () => {
+        controller.send('left', 'false');
+      });
+    rightButton &&
+      rightButton.addEventListener('mousedown', () => {
+        controller.send('right', 'true');
+      });
+      rightButton.addEventListener('mouseright', () => {
+        controller.send('right', 'false');
+      });
+    submitButton &&
+      submitButton.addEventListener('mousedown', () => {
+        controller.send('submit', 'true');
+      });
+      submitButton.addEventListener('mousesubmit', () => {
+        controller.send('submit', 'false');
+      });
+    cancelButton &&
+      cancelButton.addEventListener('mousedown', () => {
+        controller.send('cancel', 'true');
+      });
+      cancelButton.addEventListener('mousecancel', () => {
+        controller.send('cancel', 'false');
       });
 
     controller.connect();
