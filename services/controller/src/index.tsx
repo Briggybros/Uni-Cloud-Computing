83b556c5509400621b9f5e68e110e0023cb2fb5a
Gregory Sims <briggybros.2@googlemail.com>
2019-01-09 14:04:53 +0000

    Basic website setup

---
+++ services/controller/src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+
+const mount = document.getElementById('app');
+
+render(<span>Hello World!</span>, mount);
---
+++ services/controller/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.tsx'),
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)?$/,
+        loader: 'babel-loader',
+        exclude: path.resolve(__dirname, 'node_modules'),
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+      filename: 'index.html',
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: path.join(__dirname, 'src', 'static'),
+        to: path.join(__dirname, 'dist'),
+      },
+    ]),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
